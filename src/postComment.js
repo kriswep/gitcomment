d2ef56e6f87999a27ccc7bfc8a4269667682cc37
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-09-21 22:16:36 +0200

    add postComment

---
+++ src/postComment.js
@@ -0,0 +1,10 @@
+// https://api.github.com/repos/:owner/:repo/issues/:number/comments
+
+const makePostComment = ({ postJsonTo }) => ({ repo, issueNumber, token, comment }) =>
+  postJsonTo(
+    `https://api.github.com/repos/${repo}/issues/${issueNumber}/comments`,
+    token,
+    JSON.stringify({ body: comment }),
+  );
+
+export default makePostComment;
---
+++ src/postComment.test.js
@@ -0,0 +1,19 @@
+/* globals test jest expect */
+import makePostComment from './postComment';
+
+test('postComment should post Json to gh', () => {
+  const postJsonTo = jest.fn();
+  const postComment = makePostComment({
+    postJsonTo,
+  });
+
+  expect(
+    postComment.bind(null, { repo: 'repo', issueNumber: 1, token: 'token', comment: 'comment' }),
+  ).not.toThrow();
+
+  expect(postJsonTo).toHaveBeenCalledWith(
+    'https://api.github.com/repos/repo/issues/1/comments',
+    'token',
+    JSON.stringify({ body: 'comment' }),
+  );
+});
