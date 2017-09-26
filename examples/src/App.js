f90d684d7f38bc52ed2313ad25cd5a2f41689fe0
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-09-26 21:35:42 +0200

    integrate example gh token fetching

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,43 +1,60 @@
 /* globals window */
-import React from 'react';
+import React, { Component } from 'react';
+import queryString from 'query-string';
 import Gitcomment from 'gitcomment/dist/gitcomment';
 
-// class App extends Component {
-//   render() {
-//      (
-//         <Gitcomment>{name => <div>{name}</div>}</Gitcomment>
-//     );
-//   }
-// }
+const redirect = () =>
+  window.location.replace(
+    `http://github.com/login/oauth/authorize?client_id=ce7fd0d5cdbd634de3aa&scope=repo%20user&redirect_uri=${window
+      .location.href}test`,
+  );
 
-const App = () => (
-  <Gitcomment
-    repo="kriswep/gitcomment"
-    issueNumber={1}
-    token={process.env.REACT_APP_GH_TOKEN}
-    render={(loaded, comments, postComment) => {
-      const commentList = comments.map(comment => <li key={comment.id}>body: {comment.body}</li>);
-      const handler = () => {
-        postComment('test');
-      };
-      const redirect = () => {
-        window.location.replace(
-          `http://github.com/login/oauth/authorize?client_id=ce7fd0d5cdbd634de3aa&redirect_uri=${window
-            .location.href}test`,
-        );
-        // Todo: we get a code after gh is done in url query param code.
-        // with this we can get token from gatekeeper: 'http://localhost:9999/authenticate/'+code
-      };
-      return (
-        <div>
-          <p>loaded: {String(loaded)}</p>
-          <ul>{commentList}</ul>
-          <button onClick={handler}>Post sthg</button>
-          <button onClick={redirect}>Login</button>
-        </div>
-      );
-    }}
-  />
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+    };
+  }
+
+  componentDidMount() {
+    const { code } = queryString.parse(window.location.search);
+    if (code) {
+      // lets pretend we have a gatekeepr instance on localhost:9999
+      window
+        .fetch(`http://localhost:9999/authenticate/${code}`, {
+          method: 'GET',
+        })
+        .then(res => res.json())
+        .then(({ token }) => this.setState({ token }));
+    }
+  }
+
+  render() {
+    return (
+      <Gitcomment
+        repo="kriswep/gitcomment"
+        issueNumber={1}
+        token={this.state.token || process.env.REACT_APP_GH_TOKEN}
+        render={(loaded, comments, postComment) => {
+          const commentList = comments.map(comment => (
+            <li key={comment.id}>body: {comment.body}</li>
+          ));
+          const handler = () => {
+            postComment('test');
+          };
+          return (
+            <div>
+              <p>loaded: {String(loaded)}</p>
+              <ul>{commentList}</ul>
+              <button onClick={handler}>Post sthg</button>
+              <button onClick={redirect}>Login</button>
+            </div>
+          );
+        }}
+      />
+    );
+  }
+}
 
 export default App;
